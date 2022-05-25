6a450b056a6cddf88b64525cee0bfc6e09101b47
avyukti1 <82049896+avyukti1@users.noreply.github.com>
2022-05-25 14:45:27 +0530

    Create Hello_java.java

---
+++ Hello_java.java
@@ -0,0 +1,8 @@
+
+public class Hello{
+  public static void main(String[]) args {
+      //prints "Hello all, good afternoon**" in the terminal window.
+      System.out.println("Hello all, good afternoon, we are learning Jenkin-Github");
+  } 
+
+}
